20d67bb1cc1c8104bea8c4ffa72227af4bab9194
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-03-11 20:42:47 +0530

    First Commit and made the folder structure of project

---
+++ client/src/components/user/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="h-20 text-white bg-black flex items-center justify-between px-20">
+      <div className="text-2xl font-semibold">LOGO</div>
+      <div>|||</div>
+    </div>
+  )
+}
+
+export default Navbar
