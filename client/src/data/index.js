a3b4a58bdf9e4b41e362bc960e598d894d87fab3
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-03-17 22:44:19 +0530

    Restarted the project

---
+++ client/src/data/index.js
@@ -0,0 +1,21 @@
+export const navButtons = [
+  {
+    id: 1,
+    alt: "ImageButton",
+    src: "",
+    className: "",
+  },
+  {
+    id: 2,
+    alt: "ImageButton",
+    src: "",
+    className: "",
+  },
+  {
+    id: 3,
+    alt: "ImageButton",
+    src: "",
+    className: "",
+  },
+];
+
--- client/src/pages/user/Home.jsx
+++ client/src/pages/user/Home.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Navbar from "../../components/user/Navbar";
 
 const Home = () => {
   return (
     <>
-      <Navbar />
+      <div>Hello</div>
     </>
   );
 };
