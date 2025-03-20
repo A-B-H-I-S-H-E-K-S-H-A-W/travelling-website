92d67e0328f6a4e5dd2824c34a58a1956bf62ef1
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-03-21 00:28:23 +0530

    Made the loader for the home screen

--- client/src/components/common/Loading.jsx
+++ client/src/components/common/Loading.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import loader from "../../assets/png/airplane.png";
+import { motion, easeInOut, useAnimation } from "framer-motion";
 
 const Loading = () => {
-  return (
-    <div>Loading</div>
-  )
-}
+  const controls = useAnimation();
 
-export default Loading
+  useEffect(() => {
+    // Start the first animation
+    controls
+      .start({
+        z: 7,
+        opacity: 1,
+        transition: { duration: 1.7, delay: 2.8 },
+      })
+      .then(() => {
+        // Trigger the "end" animation after the first one finishes
+        controls.start({
+          opacity: 0,
+          transition: { duration: 0.5 }, // Define duration for the end animation
+        });
+      });
+  }, [controls]);
+  return (
+    <>
+      <div className="relative w-screen h-screen flex items-center justify-center overflow-hidden">
+        <motion.div
+          initial={{ z: 10, display: "block" }}
+          animate={{ z: 0, display: "none" }}
+          transition={{ duration: 5 }}
+        >
+          <motion.img
+            initial={{
+              translateX: -800,
+              width: "300px",
+              height: "300px",
+              rotate: 45,
+            }}
+            animate={{
+              translateX: 1200,
+              width: "480px",
+              height: "480px",
+              visibility: "hidden",
+            }}
+            transition={{ duration: 3.4, ease: easeInOut }}
+            src={loader}
+            className="size-80"
+            alt="img"
+          />
+        </motion.div>
+        <motion.div
+          className="absolute"
+          initial={{ z: 0, opacity: 0 }}
+          animate={controls}
+        >
+          <h1 className="md:text-6xl text-cyan-700 text-3xl font-semibold style-regular">
+            WanderSphere
+          </h1>
+        </motion.div>
+      </div>
+    </>
+  );
+};
+
+export default Loading;
--- client/src/pages/user/Home.jsx
+++ client/src/pages/user/Home.jsx
@@ -1,94 +1,109 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/user/Navbar";
 import { AnimatedImages } from "../../components/user/AnimatedImages";
 import { ButtonSolid } from "../../components/common/Button";
 import { serviceButtons } from "../../data";
+import Loading from "../../components/common/Loading";
 // import Loading from "../../components/common/Loading";
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Show the loader for 3 seconds
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer); // Cleanup the timer
+  }, []);
   return (
     <>
-      <div className="min-h-screen py-10 bg-radial-[at_90%_90%] from-sky-400 to-white to-90%">
-        <Navbar />
-        <main className="md:pt-10 pt-14 md:px-20 px-3">
-          <div className="grid md:grid-cols-2 md:space-x-5">
-            <div className="mt-16">
-              <div>
-                <h2 className="md:text-5xl text-4xl font-bold">
-                  Journey Beyond Boundaries, Where Dreams Meet{" "}
-                  <span className="style-regular text-blue-700">
-                    Destination.
-                  </span>
-                </h2>
-                <div className="mt-3">
-                  <p className="md:text-base text-xs uppercase">
-                    Discover, explore, and live your travel dreams with
-                    us—because the world is too beautiful to stay in one place.
-                    Let’s create memories that last a lifetime!
-                  </p>
-                </div>
-                <div className="mt-8">
-                  <div className="w-full h-24 rounded-2xl bg-white">
-                    <div className="grid grid-cols-4 text-center">
-                      <div className="h-24 w-full border rounded-l-2xl flex items-center justify-center">
-                        Mumbai
-                      </div>
-                      <div className="h-24 w-full border flex items-center justify-center">
-                        Kolkata
-                      </div>
-                      <div className="h-24 w-full border flex items-center justify-center">
-                        19 March
-                      </div>
-                      <div className="h-24 w-full border rounded-r-2xl flex items-center justify-center">
-                        Traveller 1
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="min-h-screen py-10 bg-radial-[at_90%_90%] from-sky-400 to-white to-90%">
+          <Navbar />
+          <main className="md:pt-10 pt-14 md:px-20 px-3">
+            <div className="grid md:grid-cols-2 md:space-x-5">
+              <div className="mt-16">
+                <div>
+                  <h2 className="md:text-5xl text-4xl font-bold">
+                    Journey Beyond Boundaries, Where Dreams Meet{" "}
+                    <span className="style-regular text-blue-700">
+                      Destination.
+                    </span>
+                  </h2>
+                  <div className="mt-3">
+                    <p className="md:text-base text-xs uppercase">
+                      Discover, explore, and live your travel dreams with
+                      us—because the world is too beautiful to stay in one
+                      place. Let’s create memories that last a lifetime!
+                    </p>
+                  </div>
+                  <div className="mt-8">
+                    <div className="w-full h-24 rounded-2xl bg-white">
+                      <div className="grid grid-cols-4 text-center">
+                        <div className="h-24 w-full border rounded-l-2xl flex items-center justify-center">
+                          Mumbai
+                        </div>
+                        <div className="h-24 w-full border flex items-center justify-center">
+                          Kolkata
+                        </div>
+                        <div className="h-24 w-full border flex items-center justify-center">
+                          19 March
+                        </div>
+                        <div className="h-24 w-full border rounded-r-2xl flex items-center justify-center">
+                          Traveller 1
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="h-12 p-1 w-full flex items-center md:space-x-4 space-x-2 bg-white mt-4 rounded-2xl">
-                    <input
-                      type="text"
-                      className="h-10 w-[80%] rounded-2xl px-2 ring ring-sky-500  text-gray-700"
-                      placeholder="Destination ?"
-                    />
-                    <ButtonSolid
-                      title={"Serach"}
-                      className={"px-9 py-2 md:py-2 rounded-2xl"}
-                    />
-                  </div>
+                    <div className="h-12 p-1 w-full flex items-center md:space-x-4 space-x-2 bg-white mt-4 rounded-2xl">
+                      <input
+                        type="text"
+                        className="h-10 w-[80%] rounded-2xl px-2 ring ring-sky-500  text-gray-700"
+                        placeholder="Destination ?"
+                      />
+                      <ButtonSolid
+                        title={"Serach"}
+                        className={"px-9 py-2 md:py-2 rounded-2xl"}
+                      />
+                    </div>
 
-                  <div className="mt-5 h-20">
-                    <div className="grid grid-cols-3 w-full gap-3 md:gap-5 rounded-2xl">
-                      {serviceButtons.map((item) => (
-                        <>
-                          <div
-                            key={item.id}
-                            className={`md:h-24 h-16 w-full hover:scale-110 duration-300 cursor-pointer ${item.className}`}
-                          >
-                            <img
-                              className={item.imageClassName}
-                              src={item.src}
-                              alt={item.alt}
-                            />
-                            <p className="font-semibold text-center mt-1 text-sm md:text-lg italic">
-                              {item.title}
-                            </p>
-                          </div>
-                        </>
-                      ))}
+                    <div className="mt-5 h-20">
+                      <div className="grid grid-cols-3 w-full gap-3 md:gap-5 rounded-2xl">
+                        {serviceButtons.map((item) => (
+                          <>
+                            <div
+                              key={item.id}
+                              className={`md:h-24 h-16 w-full hover:scale-110 duration-300 cursor-pointer ${item.className}`}
+                            >
+                              <img
+                                className={item.imageClassName}
+                                src={item.src}
+                                alt={item.alt}
+                              />
+                              <p className="font-semibold text-center mt-1 text-sm md:text-lg italic">
+                                {item.title}
+                              </p>
+                            </div>
+                          </>
+                        ))}
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="mt-20 md:mt-10 md:px-10">
-              <AnimatedImages />
-              <div className="flex items-end justify-end w-full h-44">
-                <div className="space-x-2"></div>
+              <div className="mt-20 md:mt-10 md:px-10">
+                <AnimatedImages />
+                <div className="flex items-end justify-end w-full h-44">
+                  <div className="space-x-2"></div>
+                </div>
               </div>
             </div>
-          </div>
-        </main>
-      </div>
+          </main>
+        </div>
+      )}
     </>
   );
 };
