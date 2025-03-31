9c6688900b1c7da6c58b63f74a9812a0d2f5b4ea
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-03-31 21:18:28 +0530

    Installed pakages and done error handling on mongodb function call

--- server/src/server.js
+++ server/src/server.js
@@ -5,14 +5,24 @@ import mongodbConnection from "./db/db.js";
 dotenv.config({
   path: "./env",
 });
-mongodbConnection();
+mongodbConnection()
+  .then(() => {
+    app.on("Error", (error) => {
+      console.log("Error:: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(
+        `Your server is running on http://localhost:${process.env.PORT || 3000}`
+      );
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO DB CONNECTION FAILED !!!", error);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.use("/user", userRouter);
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Your server is running on ${process.env.PORT}`);
-});
